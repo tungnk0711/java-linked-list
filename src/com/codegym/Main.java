21534528656c705c86e15db1f6c65854e01b4f39
nguyenkhanhtung <tungnk0711@gmail.com>
2018-10-09 16:37:16 +0700

    commit project

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,17 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
